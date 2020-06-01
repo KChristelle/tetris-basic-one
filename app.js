9ffa02379830d3bf4d20cdefbbb36798edec29e9
KChristelle <r.ayateke@alustudent.com>
2020-06-01 11:53:28 +0200

    Added function to move right and event listener function updated

--- app.js
+++ app.js
@@ -69,6 +69,22 @@ document.addEventListener('DOMContentLoaded', () => {
     // make the tetromino move down every second
     timerId = setInterval(moveDown, 1000)
 
+    // assign function to keyCodes
+    function control(e){
+        if(e.keyCode === 37){
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            // rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        }else if (e.keyCode === 40) {
+            moveDown()
+        }
+
+    }
+
+    document.addEventListener('keyup', control)
+
     // function to move down
     function moveDown(){
         undraw()
@@ -88,4 +104,37 @@ document.addEventListener('DOMContentLoaded', () => {
             draw()
         }
     }
+
+    // move the tetromino left, unless it is at the end of the grid
+    function moveLeft(){
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -= 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition += 1
+
+        }
+
+        draw()
+    }
+
+    // move the tetromino right, unless it is at the end of the grid
+    function moveRight(){
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -= 1
+        }
+
+        draw()
+    }
+    
+
+
+
 })
